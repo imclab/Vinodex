0c186ccb5d595f211d30cf9c0244c4821fd4c34f
Zack Grannan <zgrannan@gmail.com>
2013-04-17 14:21:27 -0700

    Added Crappy Turk API

---
+++ turk_api/job.php
@@ -0,0 +1,30 @@
+<html>
+  <body>
+<?php
+  $con = mysql_connect("127.0.0.1:3306","root","");
+  mysql_select_db("wine", $con);
+
+  if(array_key_exists("imageUrl", $_POST)) {
+    $imageUrl = mysql_real_escape_string($_POST["imageUrl"]);
+    $query = "INSERT INTO jobs(imageUrl, status) VALUES('$imageUrl','IN_PROGRESS');";
+    mysql_query($query, $con);
+  } else {
+    $result = mysql_query("SELECT id, imageUrl FROM jobs WHERE status = 'IN_PROGRESS';");
+    if (!mysql_num_rows($result)){
+      die("No Jobs!");
+    }
+    $row = mysql_fetch_assoc($result);
+    $jobId = $row["id"];
+    $imageUrl = $row["imageUrl"];
+  }
+?>
+            <img src="<?php echo $imageUrl; ?>" />
+            <form name="classify" action="job_result.php" method="POST">
+              <input type="hidden" name="jobId" value = "<?php echo $jobId; ?>">
+              <input type="text" name="name">Wine Name</input>
+              <input type="text" name="year">Wine Year</input>
+              <input type="text" name="winery">Winery</input>
+              <input type="submit">Submit</input>
+            </form>
+  </body>
+</html>
---
+++ turk_api/job_result.php
@@ -0,0 +1,28 @@
+<html>
+  <body>
+<?php
+  $con = mysql_connect("127.0.0.1:3306","root","");
+  mysql_select_db("wine", $con);
+  if($_SERVER['REQUEST_METHOD'] === 'POST'){ 
+    $jobId = mysql_real_escape_string($_POST["jobId"]);
+    $name = mysql_real_escape_string($_POST["name"]);
+    $year = mysql_real_escape_string($_POST["year"]);
+    $winery = mysql_real_escape_string($_POST["winery"]);
+    $query = "UPDATE jobs SET name='$name', year=$year, winery='$winery',".
+             "status='COMPLETED' WHERE id=$jobId LIMIT 1;";
+    mysql_query($query);
+  } elseif (array_key_exists("jobId", $_GET)) {
+    $jobId = mysql_real_escape_string($_GET["jobId"]);
+    $query = "SELECT name, year, winery FROM jobs WHERE id = $jobId AND status = 'COMPLETED'";
+    $result = mysql_query($query);
+    if (mysql_num_rows($result)){
+      $row = mysql_fetch_assoc($result);
+      $name = $row["name"];
+      $year = $row["year"];
+      $winery = $row["winery"];
+      echo "$name, $year, $winery";
+    } else {
+      echo "No results at this time";
+    }
+  } 
+?>
