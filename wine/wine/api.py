5c8995b1d56932efadf942e1f17d9c2104cb953d
Zack Grannan <zgrannan@gmail.com>
2013-04-26 20:45:54 -0700

    Added hydrate to winery

--- wine/wine/api.py
+++ wine/wine/api.py
@@ -1,6 +1,7 @@
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from wine.models import Wine, Winery
+from django.contrib.gis.geos import Point
 
 class WineResource(ModelResource):
     class Meta:
@@ -11,4 +12,11 @@ class WineryResource(ModelResource):
     class Meta:
         queryset = Winery.objects.all()
         authorization = Authorization() # TODO: Proper auth
+        excludes = ['location']
+    
+    def hydrate(self, bundle):
+        lat, lon = (bundle.data["location"]["lat"],
+                    bundle.data["location"]["lon"])
+        bundle.obj.location = Point(lat,lon)
+        return bundle
 
