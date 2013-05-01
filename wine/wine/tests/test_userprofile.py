2588692b67469c145222aa8d826e9036ba116500
Zack Grannan <zgrannan@gmail.com>
2013-04-30 20:27:03 -0700

    Finished tests for userprofile

--- wine/wine/tests/test_userprofile.py
+++ wine/wine/tests/test_userprofile.py
@@ -5,24 +5,29 @@ from unittest_helper import UnitTestHelper
 
 class UserProfileTestCase(django.test.TestCase):
 
-
-    sample_user = {
-            "username": "UPSampleUser",
-            "password": "Password",
-            "email": "sample@user.com"
-    }
+    # All users need unique username and passwords. This varible 
+    # is used to ensure that all users created have unique usernames
+    # and passwords
+    current_user_id = 0
 
     def create_user(self):
-        url = self.helper.postOK("/api/v1/auth/user/?format=json",
-                self.sample_user)
+        self.current_user_id += 1
+        sample_user = {
+                "username": "sampleUser" +str(self.current_user_id),
+                "password": "password",
+                "email": "sample" + str(self.current_user_id) + "@user.com"
+        }
 
-        return "".join(url.split("/")[1:])
+        url = self.helper.postOK("/api/v1/auth/user/?format=json", sample_user)
+
+        return "/" + "/".join(url.split("/")[3:])
 
 
-    def create_profile(self, user_url):
+    def create_profile(self):
+        user_url = self.create_user()
         profile = {
-            "firstname": "Sample",
-            "lastname": "User",
+            "first_name": "Sample",
+            "last_name": "User",
             "user": user_url
         }
         return self.helper.postOK("/api/v1/profile/?format=json", profile)
@@ -32,17 +37,20 @@ class UserProfileTestCase(django.test.TestCase):
         self.helper = UnitTestHelper(self)
 
     def test_can_create_profile(self):
-        user_url = self.create_user()
-        return self.create_profile(self.sample_user)
+        return self.create_profile()
 
-    @unittest.skip("Not implemented")
     def test_can_delete_profile(self):
-        self.fail("Not implemented")
+        return self.helper.deleteOK(self.create_profile())
 
-    @unittest.skip("Not implemented")
     def test_can_update_profile(self):
-        self.fail("Not implemented")
+        profile_url = self.create_profile()
+        self.helper.putOK(profile_url, {"first_name": "Bob"})
+        new_profile = self.helper.getOK(profile_url)
+        self.assertEquals(new_profile["first_name"], "Bob")
+        self.assertEquals(new_profile["last_name"], "User")
+        
 
-    @unittest.skip("Not implemented")
     def test_can_get_profile(self):
-        self.fail("Not implemented")
+        profile = self.helper.getOK(self.create_profile())
+        self.assertEquals(profile["first_name"], "Sample")
+        self.assertEquals(profile["last_name"], "User")
