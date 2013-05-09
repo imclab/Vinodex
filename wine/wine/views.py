0b892433ae7d209ad2c48eae95412d6984f6ae72
Zack Grannan <zgrannan@gmail.com>
2013-05-09 16:21:32 -0700

    Accept file uploads now

--- wine/wine/views.py
+++ wine/wine/views.py
@@ -9,94 +9,19 @@ from django.utils import simplejson as json
 from django.core.cache import cache
 from wine.models import Wine, Winery
 from wine.api import WineResource
+from django.views.decorators.csrf import csrf_exempt
 from tastypie.serializers import Serializer
-import zxing
+from tools import (safe_get, download_file, get_filename,
+                   get_barcode_from_image, render_result)
 
 @login_required
 def home(request):
     return render(request,"inventory.html")
 
-def safe_get(url):
-    """ Makes a GET request to the given url. If the request does not succeed,
-        then an exception is thrown """
-    response = requests.get(url)
-    response.raise_for_status()
-    return response
+def upload_image(request):
+    return render(request,"upload.html")
 
-def get_filename():
-    """ Returns a filename that can be used to store a temporary file. This file
-        is guaranteed to be available for at least two hours 
-        TODO: For now, this gives each file a .jpg extenstion, it should take
-        this as a parameter, also this should be moved to tools"""
-    if not cache.get("filename_index"):
-        cache.set("filename_index", 1, 7200)
-        return "temp/1.jpg"
-    else:
-        file_num = cache.incr("filename_index")
-        return "temp/%d.jpg" % file_num
-
-def download_file(url):
-    """ Downloads a file from the given url. Returns the filename of the downloaded
-        file"""
-    result = safe_get(url) 
-    filename = get_filename()
-    handle = open(filename, 'w')
-    handle.write(result.content)
-    return handle.name
-
-def render_wines(wines, request):
-    """
-        Returns an HTTPResponse object containing a list of wines. If the user
-        has passed in the `limit` parameter in their request, this is limited
-        to that amount. This returns this list of wines in the order they
-        were provided to this method
-    """
-    wr = WineResource()
-    rendered_wines = [wr.full_dehydrate(wr.build_bundle(obj=wine, request=request)) for wine in wines]
-    response = []
-    if request.GET.get("limit"):
-        limit = int(request.GET["limit"])
-        response = Serializer().serialize(rendered_wines[:limit])
-    else:
-        response = Serializer().serialize(rendered_wines)
-
-    return HttpResponse(response, mimetype="application/json")
-
-def get_barcode_from_image(url):
-    """
-        Returns the barcode number (as a String), analyzed from the 
-        barcode at the given url. If no barcode could be found,
-        None is returned
-    """
-    reader = zxing.BarCodeReader("/opt/local/zxing-1.6/")
-    barcode = reader.decode(url, try_harder=True)
-    if barcode and barcode.raw:
-        return barcode.raw.split()[0] # Remove trailing EOL
-    else:
-        return None
-
-def render_result(wines, wineries, request):
-    """
-        Returns the result from an OCR or label analysis. The wines are ordered
-        by likelihood which organized using two methods:
-            1. If OCR / Label analysis could figure any wineries, then wines
-               that have been identified that could be matched to a detected 
-               winery come before wines that could not be matched
-            2. In each section, the wines are ordered by popularity, as
-               determined by the wine.com API
-        If no wines could be determined, this returns a 404 message
-    """
-
-    if not wines:
-        response = {"message": "Wine could not be identified, sorry"}
-        return HttpResponseNotFound(json.dumps(response),
-                mimetype="application/json")
-
-    wines_with_wineries = [wine for wine in wines if wine.winery and wine.winery in wineries]
-    wines_without_wineries = [wine for wine in wines if wine not in
-            wines_with_wineries]
-    return render_wines(wines_with_wineries + wines_without_wineries, request)
-
+@csrf_exempt
 def wine_barcode(request):
     """
         Returns a list of wines that could be matched to a given barcode.
@@ -124,6 +49,7 @@ def wine_barcode(request):
     return render_result(wines, wineries, request)
     
      
+@csrf_exempt
 def wine_ocr(request):
     """
         Returns a list of wines that could be matched to a given barcode.
@@ -132,16 +58,24 @@ def wine_ocr(request):
         uploaded in FILES.
     """
     def get_url(request):
-        if not request.GET.get("url"):
-            response = {"message": "The parameter `url` is required"}
+        if request.GET.get("url"):
+            url = request.GET.get("url")
+            filename = download_file("url")
+            return filename, None
+        elif request.FILES.get("image"):
+            image_file = request.FILES["image"]
+            image_filename = get_filename()
+            output_file = open(image_filename, 'w')
+            output_file.write(image_file.read())
+            return image_filename, None
+        elif not request.GET.get("url"):
+            response = {"message": "A `url` or an `image` is required"}
             return None, HttpResponseBadRequest(json.dumps(response),
                     mimetype="application/json")
-        return request.GET["url"], None
 
-    url, response = get_url(request)
-    if not url:
+    filename, response = get_url(request)
+    if not filename:
         return response
-    filename = download_file(url)
     wines = Wine().identify_from_label(filename)
     wineries = Winery().identify_from_label(filename)
     return render_result(wines, wineries, request)
