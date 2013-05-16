e0b69043f105aed216839d06bf076105c458963b
Luke Cycon <lcycon@gmail.com>
2013-05-15 21:12:44 -0700

    Change pass

--- wine/wine/settings/production.py
+++ wine/wine/settings/production.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cse110_dev', # Or path to database file if using sqlite3.
+        'NAME': 'cse110_prod', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'cse110app',
-        'PASSWORD': 'apppass',
+        'PASSWORD': 'cse110pass',
         'HOST': 'ec2-54-225-44-160.compute-1.amazonaws.com', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '5432', # Set to empty string for default.
     }
